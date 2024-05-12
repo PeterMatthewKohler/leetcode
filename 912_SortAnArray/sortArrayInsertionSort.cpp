e26126d85cf85213d71954ac465ebf31569d5b9a
PeterMatthewKohler <pmkohler@gmail.com>
2024-05-12 12:56:26 -0600

    Add 912: Sort an Array solution using insertion sort algorithm

---
+++ 912_SortAnArray/sortArrayInsertionSort.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        // Use insertion sort
+        for(int i = 1; i < nums.size(); i++)
+        {
+            int j = i - 1;
+            while(j >= 0 && nums[j+1] < nums[j])
+            {
+                int tmp = nums[j+1];
+                nums[j+1] = nums[j];
+                nums[j] = tmp;
+                j--;
+            }
+        }
+        return nums;
+    }
+};
